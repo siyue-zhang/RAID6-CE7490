197b43dae14ffbc7c9165db52d7ef21d1a7e608a
Heisenberg-Yin <545068655@qq.com>
2022-11-16 15:54:32 +0800

    update

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ from src.config import Config
 from src.RAID6 import RAID6
 import os
 from data import DATA_PATH
+import time
 
+T1 = time.time()
 cfg = Config()
 dir=cfg.mkdisk('./')
 test=RAID6(cfg)
 filename='txt_test.txt'
 test.write_to_disk(os.path.join(DATA_PATH, filename), dir)
+T2 = time.time()
+print('程序运行时间:%s毫秒' % ((T2 - T1)*1000))
